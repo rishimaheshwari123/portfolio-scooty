9e028d1887e82323e301159d15c9f7bce490e7b1
Rishi <rishimaheshwari040@gmail.com>
2024-05-07 11:22:49 +0530

    services

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,14 +6,15 @@ import { useSelector } from "react-redux";
 import OpenRoute from "./components/Admin/auth/OpenRoute";
 import PrivateRoute from "./components/Admin/auth/PrivateRoute";
 import Dashboard from "./components/Admin/Sidebar/Dashboard";
-import Footer from "./components/footer/Footer"
-import Scroll from "./components/scrollbar/Scroll"
-import Contact from "./pages/contact/Contact"
+import Footer from "./components/footer/Footer";
+import Scroll from "./components/scrollbar/Scroll";
+import Contact from "./pages/contact/Contact";
 //Admin Routes
 import Login from "./components/Admin/Login";
 import Download from "./components/Admin/Sidebar/Download";
+import Sercives from "./pages/service/Sercives";
 const App = () => {
-  const { token } = useSelector((state) => state.auth)
+  const { token } = useSelector((state) => state.auth);
 
   return (
     <BrowserRouter>
@@ -21,33 +22,33 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Sercives />} />
 
-        
-        <Route path="/admin/login" element={
-        <OpenRoute>
-           <Login />
+        <Route
+          path="/admin/login"
+          element={
+            <OpenRoute>
+              <Login />
+            </OpenRoute>
+          }
+        />
 
-        </OpenRoute>
-        
-        } />
-
-<Route
+        <Route
           element={
             <PrivateRoute>
               <Dashboard />
             </PrivateRoute>
           }
-        > 
-        
-        <Route path="/admin/dashboard" 
-          element={
-            <PrivateRoute>
-    <Download/>
-            </PrivateRoute>
-          }
-        />
+        >
+          <Route
+            path="/admin/dashboard"
+            element={
+              <PrivateRoute>
+                <Download />
+              </PrivateRoute>
+            }
+          />
         </Route>
-
       </Routes>
       <Scroll />
       <Footer />
---
+++ client/src/pages/service/Sercives.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sercives = () => {
+  return (
+    <div>
+      <h1>Sercives</h1>
+    </div>
+  );
+};
+
+export default Sercives;
